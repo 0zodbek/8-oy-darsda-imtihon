053a45974baea0f3e3ab46e30fecc2fa9b0e35f7
0zodbek <ozpdbekov@gmail.com>
2024-11-30 16:46:12 +0500

    scroll-x remove

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,29 @@ import Knowladge from './components/Know'
 import Gerbs from './assets/logo.svg'
 import Pragment from "./assets/leftGerb.svg"
 import './App.css'
-// import Card from "./components/ui/Card"
 const App: FC = () => {
 
-  return ( 
-    <div className=''>
+  return (
+    <div className='w-[1440px] mx-auto'>
       <div className='flex mb-10 w-[1440px] pl-[64px] mx-auto'>
-      <div className="w-72 h-13 grid gap-2">
-        <img
-          src={Gerbs}
-          alt="Logo"
-          className="w-16 h-16 object-contain ml-16"
-        />
-        <h4 className="text-black w-48 h-12 text-center">
-          Давлат хизматининг ягона электрон ахборот-таҳлил тизими
-        </h4>
-      </div>
-      <div>
-      <p className='w-[642px] font-semibold text-3xl text-center mt-10'>
-       Республика Ассессмент маркази онлайн платформаси
-       </p>
-      </div>
-      <div className='mt-10 ml-32'>
-        <img src={Pragment} alt="" />
-      </div>
+        <div className="w-72 h-13 grid gap-2">
+          <img
+            src={Gerbs}
+            alt="Logo"
+            className="w-16 h-16 object-contain ml-16"
+          />
+          <h4 className="text-black w-48 h-12 text-center">
+            Давлат хизматининг ягона электрон ахборот-таҳлил тизими
+          </h4>
+        </div>
+        <div>
+          <p className='w-[642px] font-semibold text-3xl text-center mt-10'>
+            Республика Ассессмент маркази онлайн платформаси
+          </p>
+        </div>
+        <div className='mt-10 ml-32'>
+          <img src={Pragment} alt="" />
+        </div>
       </div>
       {/* <Gauge></Gauge> */}
       <PersonPage />
